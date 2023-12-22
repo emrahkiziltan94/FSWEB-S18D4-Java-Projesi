112a64ec9432f7ef534d9736cf1d4ff492ab8aa5
mrhkzltn <emrahkiziltan94@gmail.com>
2023-12-22 16:55:05 +0300

    s18d4 solution.

---
+++ src/main/java/com/workintech/s18d4/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package com.workintech.s18d4.controller;
+
+import com.workintech.s18d4.dto.CustomerResponse;
+import com.workintech.s18d4.entity.Customer;
+import com.workintech.s18d4.service.CustomerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    private final CustomerService customerService;
+
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+
+    }
+
+    @PostMapping
+    public CustomerResponse save(@RequestBody Customer c){
+        Customer saved = this.customerService.save(c);
+        return new CustomerResponse(saved.getId(), saved.getEmail(), saved.getSalary());
+
+    }
+}
